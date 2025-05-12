77b28f47f2aa24502eb8fdf94ae72876b6047ec7
PythonTester <gdenisse971@outlook.fr>
2025-05-12 16:48:05 +0200

    infos footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,7 +1,16 @@
 <footer>
     <div id="contact-infos">
+        <p>L'ATELIER DU BOIS</p>
+        <hr>
+        <p>909, avenue de Bruneau ● 33690, Jacques-sur-Berger</p>
+
+        <p>jacques.thomas@yahoo.fr</p>
+
+        <p>+33 07 69 56 72 05</p>
+
 
     </div>
+
     <div id="legal-infos">
         <p>&copy; 2024 L'Atelier du Bois. Tous droits réservés.</p>
     </div>
